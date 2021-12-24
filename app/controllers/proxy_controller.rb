3302dd2d82d1bd0dae15de7a727f7e7f829445c5
LayV <myakinb@mail.ru>
2021-12-24 20:58:40 +0300

    for_pr

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -1,2 +1,52 @@
+require 'nokogiri'
+require 'open-uri'
+
+
 class ProxyController < ApplicationController
+  before_action :parse_params, only: :output
+  before_action :prepare_url, only: :output
+
+  def input; end
+
+  def output
+    api_response = URI.open(@url)
+
+    case @side
+    when 'server'
+      @result = xslt_transform(api_response).to_html
+    when 'client-with-xslt'
+      render xml: insert_browser_xslt(api_response).to_xml
+    else
+      render xml: api_response
+    end
+  end
+
+  private
+
+  BASE_API_URL           = 'http://localhost:3000/?format=xml'.freeze
+  XSLT_SERVER_TRANSFORM  = "#{Rails.root}/public/server_transform.xslt".freeze
+  XSLT_BROWSER_TRANSFORM = '/browser_transform.xslt'.freeze
+
+  def parse_params
+    @number = params[:number]
+  end
+
+  def prepare_url
+    @url = BASE_API_URL + "&range=#{@number}"
+  end
+
+  def xslt_transform(result, transform: XSLT_SERVER_TRANSFORM)
+    doc = Nokogiri::XML(result)
+    xslt = Nokogiri::XSLT(File.read(transform))
+    xslt.transform(doc)
+  end
+
+  def insert_browser_xslt(result, transform: XSLT_BROWSER_TRANSFORM)
+    doc = Nokogiri::XML(result)
+    xslt = Nokogiri::XML::ProcessingInstruction.new(doc,
+                                                    'xml-stylesheet',
+                                                    "type=\"text/xsl\" href=\"#{transform}\"")
+    doc.root.add_previous_sibling(xslt)
+    doc
+  end
 end
